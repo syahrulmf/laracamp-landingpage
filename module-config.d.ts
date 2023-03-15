affcdee9621fbd4abb20397aa286bddb480840c5
syahrulmf <syahrulmf@gmail.com>
2023-03-15 22:41:54 +0700

    Slicing Featured Section

--- module-config.d.ts
+++ module-config.d.ts
@@ -1,6 +1,7 @@
 declare module "react-landing-storybook/dist/components/Navbar";
 declare module "react-landing-storybook/dist/components/Button";
 declare module "react-landing-storybook/dist/components/FeaturedItems";
+declare module "react-landing-storybook/dist/components/FeaturedSection";
 
 declare module "*.svg" {
   import React = require("react");
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 import Button from "react-landing-storybook/dist/components/Button";
+import FeaturedSection from "react-landing-storybook/dist/components/FeaturedSection";
 import Navbar from "react-landing-storybook/dist/components/Navbar";
 
 export default function Home() {
@@ -59,6 +60,75 @@ export default function Home() {
         <Hero />
         <Partnership />
         <Features />
+
+        <FeaturedSection
+          badge={{
+            content: "Before taking any program it is good to choose the path",
+            title: "Interview",
+          }}
+          button={
+            <Button state="secondary">
+              <a href="">Learn More</a>
+            </Button>
+          }
+          content={{
+            heading: "Prepare The Journey",
+            paragraph:
+              "We do really care to our students’ future career so it would be good if you are taking a quick interview",
+            preheading: "better carrier",
+          }}
+          image="/images/image-1.jpg"
+          number={{
+            className: "bg-yellow",
+            text: "1.",
+          }}
+        />
+
+        <FeaturedSection
+          badge={{
+            content: "Study and follow the journey based on your career path",
+            title: "Focus",
+          }}
+          button={
+            <Button state="secondary">
+              <a href="">View Demo</a>
+            </Button>
+          }
+          content={{
+            heading: "Finish The Project",
+            paragraph:
+              "Each of you will be joining the private group and also working together with team members on project",
+            preheading: "study harder",
+          }}
+          image="/images/image-2.jpg"
+          number={{
+            className: "bg-purple",
+            text: "2.",
+          }}
+        />
+
+        <FeaturedSection
+          badge={{
+            content: "Demonstration for your final project to the judges",
+            title: "Final Gate",
+          }}
+          button={
+            <Button state="secondary">
+              <a href="">Showcase</a>
+            </Button>
+          }
+          content={{
+            heading: "Big Demo Day",
+            paragraph:
+              "Learn how to speaking in public to demonstrate your final project and receive the important feedbacks",
+            preheading: "end game",
+          }}
+          image="/images/image-3.jpg"
+          number={{
+            className: "bg-green",
+            text: "3.",
+          }}
+        />
       </main>
     </>
   );
